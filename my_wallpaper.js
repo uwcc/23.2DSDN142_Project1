227d26a7211ca80410f299abd942b2d560a81f4a
Phoebe Zeller <43123953+uwcc@users.noreply.github.com>
2025-07-18 16:17:41 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -5,6 +5,8 @@ let rect_height = 30;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
+  //pWallpaper.output_mode(GRID_WALLPAPER);
+  
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
